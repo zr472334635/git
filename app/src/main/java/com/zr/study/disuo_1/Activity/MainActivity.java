4835748332eb7bf6a6668279f59ad98d7156c679
duray <472334635@qq.com>
2017-11-30 16:06:49 +0800

    note_duray 2017/11/30

--- app/src/main/java/com/zr/study/disuo_1/Activity/MainActivity.java
+++ app/src/main/java/com/zr/study/disuo_1/Activity/MainActivity.java
@@ -36,9 +36,11 @@ public class MainActivity extends SlidingFragmentActivity {
 
     private ImageView ibtn_icon_user;
     private ImageView msg;
-    private RelativeLayout relativeLayout;
+    private RelativeLayout rl_main_bottom;
+    private RelativeLayout rl_main_top;
     private RelativeLayout rl;
-    private ImageView arrow;
+    private ImageButton toparrow;
+    private ImageView bottomarrow;
     SlidingMenu slidingMenu;
     private RelativeLayout.LayoutParams params;
     private boolean isOpen = false; //是否开启状态
@@ -62,12 +64,14 @@ public class MainActivity extends SlidingFragmentActivity {
 
 
         ibtn_icon_user= (ImageView) findViewById(R.id.iv_main_user);
-        relativeLayout= (RelativeLayout) findViewById(R.id.rl_main_rl1);
-        arrow= (ImageView) findViewById(R.id.iv_main_arrow);
+        rl_main_top= (RelativeLayout) findViewById(R.id.rl_main_top);
+        rl_main_bottom= (RelativeLayout) findViewById(R.id.rl_main_bottom);
+        toparrow= (ImageButton) findViewById(R.id.iv_main_toparrow);
+        bottomarrow= (ImageView) findViewById(R.id.iv_main_bottomarrow);
         msg= (ImageView) findViewById(R.id.iv_main_msg);
         rl= (RelativeLayout) findViewById(R.id.rl_main_rl);
 
-        params= (RelativeLayout.LayoutParams) relativeLayout.getLayoutParams();
+        params= (RelativeLayout.LayoutParams) rl_main_bottom.getLayoutParams();
         DisplayMetrics metrics=new DisplayMetrics();
         getWindowManager().getDefaultDisplay().getMetrics(metrics);
         final int width=metrics.widthPixels;
@@ -75,14 +79,13 @@ public class MainActivity extends SlidingFragmentActivity {
         final int densityDpi=metrics.densityDpi;
         oldHeight=height-densityDpi*250/160;
         params.topMargin=oldHeight;
-        relativeLayout.setLayoutParams(params);
+        rl_main_bottom.setLayoutParams(params);
 
-
-        initShowHide();
+        initShowTopHide();
+        initShowBottomHide();
         ibtn_icon_user.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-//                slidingMenu.showMenu();
                 slidingMenu.toggle();
             }
         });
@@ -91,13 +94,13 @@ public class MainActivity extends SlidingFragmentActivity {
         x.view().inject(this);
     }
 
-    private void initShowHide() {
+    private void initShowTopHide() {
         //布局完成
-        relativeLayout.getViewTreeObserver().addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
+        rl_main_top.getViewTreeObserver().addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
             @Override
             public void onGlobalLayout() {
                 //移除所有监听
-                relativeLayout.getViewTreeObserver().removeGlobalOnLayoutListener(this);
+                rl_main_top.getViewTreeObserver().removeGlobalOnLayoutListener(this);
                 //mLayoutHeight = relativeLayout.getHeight();
                 System.out.println("得到的高度：" + oldHeight);
                 //隐藏当前控件
@@ -106,7 +109,7 @@ public class MainActivity extends SlidingFragmentActivity {
         });
 
         //点击,开始执行动画
-        arrow.setOnClickListener(new View.OnClickListener() {
+        toparrow.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(final View view) {
                 ValueAnimator valueAnimator = new ValueAnimator();
@@ -123,7 +126,7 @@ public class MainActivity extends SlidingFragmentActivity {
                     public void onAnimationUpdate(ValueAnimator animator) {
                         int value = (int) animator.getAnimatedValue();
                         params.topMargin=value;
-                        relativeLayout.setLayoutParams(params);
+                        rl_main_bottom.setLayoutParams(params);
 
                     }
                 });
@@ -132,12 +135,12 @@ public class MainActivity extends SlidingFragmentActivity {
                     @Override
                     public void onAnimationStart(Animator animator) {
                         //动画开始，不能点击
-                        arrow.setClickable(false);
+                        toparrow.setClickable(false);
                     }
 
                     @Override
                     public void onAnimationEnd(Animator animator) {
-                        arrow.setClickable(true);
+                        toparrow.setClickable(true);
                         if(isOpen){
                             ibtn_icon_user.setVisibility(View.INVISIBLE);
                             msg.setVisibility(View.INVISIBLE);
@@ -170,10 +173,97 @@ public class MainActivity extends SlidingFragmentActivity {
                 }
 
                 //进行旋转
-                ViewCompat.animate(arrow).rotationBy(180f).setDuration(500).start();
+                ViewCompat.animate(toparrow).rotationBy(180f).setDuration(500).start();
             }
         });
     }
+
+
+    private void initShowBottomHide() {
+        //布局完成
+        rl_main_bottom.getViewTreeObserver().addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
+            @Override
+            public void onGlobalLayout() {
+                //移除所有监听
+                rl_main_bottom.getViewTreeObserver().removeGlobalOnLayoutListener(this);
+                //mLayoutHeight = relativeLayout.getHeight();
+                System.out.println("得到的高度：" + oldHeight);
+                //隐藏当前控件
+                //relativeLayout.setPadding(0,-mLayoutHeight,0,0);
+            }
+        });
+
+        //点击,开始执行动画
+        bottomarrow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(final View view) {
+                ValueAnimator valueAnimator = new ValueAnimator();
+                if (isOpen){
+                    valueAnimator.setIntValues(height-200,oldHeight);
+
+                }else {
+                    valueAnimator.setIntValues(oldHeight,height-200);
+
+                }
+                //设置监听的值
+                valueAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+                    @Override
+                    public void onAnimationUpdate(ValueAnimator animator) {
+                        int value = (int) animator.getAnimatedValue();
+                        params.topMargin=value;
+                        rl_main_bottom.setLayoutParams(params);
+
+                    }
+                });
+                //动画执行中监听
+                valueAnimator.addListener(new Animator.AnimatorListener() {
+                    @Override
+                    public void onAnimationStart(Animator animator) {
+                        //动画开始，不能点击
+                        bottomarrow.setClickable(false);
+                    }
+
+                    @Override
+                    public void onAnimationEnd(Animator animator) {
+                        bottomarrow.setClickable(true);
+                        if(isOpen){
+                            ibtn_icon_user.setVisibility(View.INVISIBLE);
+                            msg.setVisibility(View.INVISIBLE);
+                            rl.setVisibility(View.INVISIBLE);
+
+                        }
+                    }
+
+                    @Override
+                    public void onAnimationCancel(Animator animator) {
+
+                    }
+
+                    @Override
+                    public void onAnimationRepeat(Animator animator) {
+
+                    }
+                });
+                isOpen = !isOpen;
+                valueAnimator.setDuration(500);
+                valueAnimator.start();
+                //状态更改
+
+                //Toast.makeText(MainActivity.this,"isOpen="+isOpen,Toast.LENGTH_SHORT).show();
+
+                if(!isOpen){
+                    ibtn_icon_user.setVisibility(View.VISIBLE);
+                    msg.setVisibility(View.VISIBLE);
+                    rl.setVisibility(View.VISIBLE);
+                }
+
+                //进行旋转
+                ViewCompat.animate(bottomarrow).rotationBy(180f).setDuration(500).start();
+            }
+        });
+    }
+
+
 
 
     private void initFragment() {
