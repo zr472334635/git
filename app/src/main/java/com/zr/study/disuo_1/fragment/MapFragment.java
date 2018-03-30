ae80f3b4158abac23a346d7261894050f32a4c9b
zr472334635 <472334635@qq.com>
2018-03-30 22:38:21 +0800

    2018-3-30

--- app/src/main/java/com/zr/study/disuo_1/fragment/MapFragment.java
+++ app/src/main/java/com/zr/study/disuo_1/fragment/MapFragment.java
@@ -10,6 +10,7 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.amap.api.maps2d.AMap;
 import com.amap.api.maps2d.CameraUpdateFactory;
@@ -30,6 +31,7 @@ import com.amap.api.services.route.DriveRouteResult;
 import com.amap.api.services.route.RideRouteResult;
 import com.amap.api.services.route.RouteSearch;
 import com.amap.api.services.route.WalkRouteResult;
+import com.zr.study.disuo_1.Activity.MainActivity;
 import com.zr.study.disuo_1.R;
 
 /**
@@ -102,7 +104,7 @@ public class MapFragment extends Fragment implements AMap.OnMyLocationChangeList
         MyLocationStyle myLocationStyle;
         myLocationStyle = new MyLocationStyle();//初始化定位蓝点样式类myLocationStyle.myLocationType(MyLocationStyle.LOCATION_TYPE_LOCATION_ROTATE);//连续定位、且将视角移动到地图中心点，定位点依照设备方向旋转，并且会跟随设备移动。（1秒1次定位）如果不设置myLocationType，默认也会执行此种模式。
         myLocationStyle.myLocationType(MyLocationStyle.LOCATION_TYPE_FOLLOW);
-        myLocationStyle.interval(2000); //设置连续定位模式下的定位间隔，只在连续定位模式下生效，单次定位模式下不会生效。单位为毫秒。
+        myLocationStyle.interval(20000); //设置连续定位模式下的定位间隔，只在连续定位模式下生效，单次定位模式下不会生效。单位为毫秒。
         aMap.setMyLocationStyle(myLocationStyle);//设置定位蓝点的Style
         aMap.getUiSettings().setMyLocationButtonEnabled(true);//设置默认定位按钮是否显示，非必需设置。
         aMap.setMyLocationEnabled(true);// 设置为true表示启动显示定位蓝点，false表示隐藏定位蓝点并不进行定位，默认是false。\
@@ -179,22 +181,50 @@ public class MapFragment extends Fragment implements AMap.OnMyLocationChangeList
 
     }
 
+//    public void getMarker(AMap aMap) {
+//        double[] Latitude={39.906901,40.906901,38.906901};
+//        double[] Longitude={116.397972,117.397972,118.397972};
+//        for(int i=0;i<3;i++) {
+//            LatLng latLng = new LatLng(Latitude[i], Longitude[i]);
+//            final Marker marker = aMap.addMarker(new MarkerOptions().position(latLng).title("北京").snippet("DefaultMarker"));
+//            // 定义 Marker 点击事件监听
+//            AMap.OnMarkerClickListener markerClickListener = new AMap.OnMarkerClickListener() {
+//                // marker 对象被点击时回调的接口
+//                // 返回 true 则表示接口已响应事件，否则返回false
+//                @Override
+//                public boolean onMarkerClick(Marker marker) {
+//                    Log.e("你是一个好人", "你是一个好人:" );
+//                    Toast.makeText(getContext(),"你是一个好人",Toast.LENGTH_SHORT).show();
+//                    return true;
+//                }
+//            };
+//        }
+//        aMap.setOnMarkerClickListener(markerClickListener);
+//
+//
+//    }
+
     public void getMarker(AMap aMap) {
-        LatLng latLng = new LatLng(39.906901,116.397972);
-        final Marker marker = aMap.addMarker(new MarkerOptions().position(latLng).title("北京").snippet("DefaultMarker"));
-        // 定义 Marker 点击事件监听
-        AMap.OnMarkerClickListener markerClickListener = new AMap.OnMarkerClickListener() {
-            // marker 对象被点击时回调的接口
-            // 返回 true 则表示接口已响应事件，否则返回false
-            @Override
-            public boolean onMarkerClick(Marker marker) {
-                return false;
-            }
-        };
+
+            LatLng latLng = new LatLng(32.344486, 118.397267);
+            final Marker marker = aMap.addMarker(new MarkerOptions().position(latLng).title("北京").snippet("DefaultMarker"));
+            // 定义 Marker 点击事件监听
+            AMap.OnMarkerClickListener markerClickListener = new AMap.OnMarkerClickListener() {
+                // marker 对象被点击时回调的接口
+                // 返回 true 则表示接口已响应事件，否则返回false
+                @Override
+                public boolean onMarkerClick(Marker marker) {
+
+                    Log.e("你是一个好人", "你是一个好人:" );
+                    Toast.makeText(getContext(),"你是一个好人",Toast.LENGTH_SHORT).show();
+                    return false;
+                }
+            };
+        aMap.setOnMarkerClickListener(markerClickListener);
+
 
 
     }
-
 
     @Override
     public View getInfoWindow(Marker marker) {
